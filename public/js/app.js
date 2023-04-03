750eacbea8bf5dd57afdb9022b9db025e1095064
Kuh <73922679+ramadhankukuh@users.noreply.github.com>
2023-04-03 14:11:15 +0700

    Create app.js

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+import { disabledButton } from "./disabledButton.js";
+import { getUrl } from "./getUrl.js";
+
+let app = document.getElementById('app');
+let form = document.getElementById('form');
+let content = document.getElementById('content');
+
+disabledButton(); // disable button at the beginning and with empty input
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    content.innerHTML = '<img src="./img/spinning-circles.svg" alt="loader" />';
+
+    let url = e.target.url.value; // get value of input
+    let domain = url.split('/')[2]; // get domain
+
+    if(domain === 'www.tiktok.com' || domain === 'vm.tiktok.com' || domain === 'vt.tiktok.com'){
+        getUrl(url); // get data video
+    }else{
+        content.innerHTML = '<h3 class="messageError">Error, URL Bukan Link TikTok</h3>'
+    }
+    e.target.reset();
+})
+
